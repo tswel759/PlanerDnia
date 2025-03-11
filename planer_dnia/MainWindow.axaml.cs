9a7956baa7ceeb6f071c49a231d62e27625f87c4
szyrad <@dfd>
2025-03-11 09:29:31 +0100

    zrobienie grida

--- planer_dnia/MainWindow.axaml.cs
+++ planer_dnia/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace planer_dnia;
 
@@ -7,5 +8,15 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Zadanie(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Usun(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 }
