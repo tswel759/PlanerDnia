fe30e6e0745d8e16d9dfb7dba6ee4bedb3133ebf
pc <szymon.radomski@cyfroweszkoly.pl>
2025-03-23 21:22:08 +0100

    ukonczenie zadania

--- planer_dnia/MainWindow.axaml.cs
+++ planer_dnia/MainWindow.axaml.cs
@@ -7,27 +7,102 @@ namespace planer_dnia
     public partial class MainWindow : Window
     {
         private List<Zadanie> zadania = new();
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void DodajZadanie(object? sender, RoutedEventArgs e)
+        private void DodajZadanie(object sender, RoutedEventArgs e)
         {
-            if (NazwaZadania.Text != "" && KategoriaZadania.SelectedItem is ComboBoxItem kat)
+            if (NazwaZadania.Text != "")
             {
-                zadania.Add(new Zadanie { Nazwa = NazwaZadania.Text, Kategoria = kat.Content.ToString() });
+                var noweZadanie = new Zadanie();
+                noweZadanie.Nazwa = NazwaZadania.Text;
+                if (KategoriaZadania.SelectedItem is ComboBoxItem kat)
+                {
+                    noweZadanie.Kategoria = kat.Content.ToString();
+                }
+                else
+                {
+                    noweZadanie.Kategoria = "Brak";
+                }
+
+                zadania.Add(noweZadanie);
                 OdswiezListe();
             }
         }
-        private void Zadanie(object? sender, RoutedEventArgs e)
+
+
+        private void UsunZadanie(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            Button przycisk = (Button)sender;
+            if (przycisk.Tag != null)
+            {
+                zadania.Remove((Zadanie)przycisk.Tag);
+                OdswiezListe();
+            }
         }
 
-        private void Usun(object? sender, RoutedEventArgs e)
+        private void OdswiezListe()
         {
-            throw new System.NotImplementedException();
+            ListaZadan.Children.Clear();
+            ListaZadan.RowDefinitions.Clear(); 
+
+            for (int i = 0; i < zadania.Count; i++)
+            {
+                ListaZadan.RowDefinitions.Add(new RowDefinition()); 
+
+                var zadanie = zadania[i];
+
+                var nazwa = new TextBlock();
+                nazwa.Text = zadanie.Nazwa;
+                Grid.SetRow(nazwa, i);
+                Grid.SetColumn(nazwa, 0);
+                ListaZadan.Children.Add(nazwa);
+
+                var kategoria = new TextBlock();
+                kategoria.Text = zadanie.Kategoria;
+                Grid.SetRow(kategoria, i);
+                Grid.SetColumn(kategoria, 1);
+                ListaZadan.Children.Add(kategoria);
+
+                var checkBox = new CheckBox();
+                checkBox.IsChecked = zadanie.CzyUkonczone;
+                checkBox.Checked += (s, e) => { zadanie.CzyUkonczone = checkBox.IsChecked ?? false; };
+                Grid.SetRow(checkBox, i);
+                Grid.SetColumn(checkBox, 2);
+                ListaZadan.Children.Add(checkBox);
+
+                var btnUsun = new Button();
+                btnUsun.Content = "Usuń";
+                btnUsun.Tag = zadanie;
+                btnUsun.Click += UsunZadanie;
+                Grid.SetRow(btnUsun, i);
+                Grid.SetColumn(btnUsun, 3);
+                ListaZadan.Children.Add(btnUsun);
+            }
+
+            int ukonczone = 0;
+            for (int i = 0; i < zadania.Count; i++)
+            {
+                if (zadania[i].CzyUkonczone)
+                {
+                    ukonczone++;
+                }
+            }
+
+            Podsumowanie.Text = "Zadań: " + zadania.Count + ", Ukończonych: " + ukonczone;
+        }
+
+
+
+
+        public class Zadanie
+        {
+            public string Nazwa { get; set; }
+            public string Kategoria { get; set; }
+            public bool CzyUkonczone { get; set; }
         }
     }
 }
