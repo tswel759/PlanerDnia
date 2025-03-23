c54f7bcd1ff2b70bb70e7edd91d141851194b865
pc <szymon.radomski@cyfroweszkoly.pl>
2025-03-23 20:27:43 +0100

    stwworzenie listy

--- planer_dnia/MainWindow.axaml.cs
+++ planer_dnia/MainWindow.axaml.cs
@@ -1,22 +1,27 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace planer_dnia;
-
-public partial class MainWindow : Window
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
-    }
+        private List<Zadanie> zadania = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            AktualizujWidok();
+        }
 
-    private void Zadanie(object? sender, RoutedEventArgs e)
-    {
-        throw new System.NotImplementedException();
-    }
+        private void Zadanie(object? sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
 
-    private void Usun(object? sender, RoutedEventArgs e)
-    {
-        throw new System.NotImplementedException();
+        private void Usun(object? sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
